e97e084879910f608edd7c4623661cde1ce3988c
Chase Simmons <csim6890@gmail.com>
2021-04-16 20:52:53 -0500

    datatypes

--- CS_re_Intro/Program.cs
+++ CS_re_Intro/Program.cs
@@ -5,6 +5,8 @@ namespace CS_re_Intro
     class Program
     {
         static void Main(string[] args)
+
+            // VARS & CONSOLE
         {
             string name = "John";
 
@@ -13,7 +15,21 @@ namespace CS_re_Intro
             Console.WriteLine(name);
             Console.WriteLine(age);
 
+            Console.Clear();
+
+            // DATATYPES
+
+            string phrase = "Hello World"; // STRING
+            char grade = 'A'; // SINGLE STRING CHARACTER
+            int age2 = 30; // INTEGER
+            float floater = 30; // LEAST ACURATE DECIMAL TYPE
+            double doubler = 30.22; // MID RANGE ACURATE DECIMAL TYPE
+            decimal decimalNum = 21; // MOST ACCURATE DECIMAL TYPE
+            bool tF = true; // TRUE || FALSE
+
+
             Console.ReadLine();
+
         }
     }
 }
