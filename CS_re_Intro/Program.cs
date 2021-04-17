362597eecc30bca6d5b7722ba509e4efad3c3679
Chase Simmons <csim6890@gmail.com>
2021-04-16 20:34:10 -0500

    first commit

---
+++ CS_re_Intro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CS_re_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
